ea9efd725390db7c6297d6dc46a086b1bfe3a024
rafikusdiarto <rafikusdiarto1707@gmail.com>
2023-06-25 19:37:46 +0700

    add:grafik income admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Order;
 use App\Models\Product;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
@@ -16,6 +18,7 @@ class DashboardController extends Controller
 
     public function index(){
         try {
+
             $countUser = User::whereHas('roles', function($thisRole){
                 $thisRole->where('name', 'user');
             })->count();
@@ -23,6 +26,19 @@ class DashboardController extends Controller
             $this->param ['countProduct'] = Product::count();
             $this->param ['countUser'] = $countUser;
 
+            $this->param['getIncomeJanuary'] = Order::whereMonth('updated_at', '01')->where('status', '=', 'paid')->sum('total_price');
+            $this->param['getIncomeFebruary'] = Order::whereMonth('updated_at', '02')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeMarch'] = Order::whereMonth('updated_at', '03')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeApril'] = Order::whereMonth('updated_at', '04')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeMay'] = Order::whereMonth('updated_at', '05')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeJune'] = Order::whereMonth('updated_at', '06')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeJuly'] = Order::whereMonth('updated_at', '07')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeAugust'] = Order::whereMonth('updated_at', '08')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeSeptember'] = Order::whereMonth('updated_at', '09')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeOctober'] = Order::whereMonth('updated_at', '10')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeNovember'] = Order::whereMonth('updated_at', '11')->where('status', '=','paid')->sum('total_price');
+            $this->param['getIncomeDecember'] = Order::whereMonth('updated_at', '12')->where('status', '=','paid')->sum('total_price');
+            $this->param['countAllIncome'] = Order::where('status', '=', 'paid')->sum('total_price');
             return view('admin.pages.dashboard', $this->param);
         } catch (\Exception $e) {
             return redirect()->back()->withError($e->getMessage());
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -100,64 +100,50 @@
           </div>
         </div>
       </div>
-      <!-- Total Revenue -->
+      <!-- Total Income -->
       <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
         <div class="card">
-          <div class="row row-bordered g-0">
-            <div class="col-md-8">
-              <h5 class="card-header m-0 me-2 pb-3">Total Revenue</h5>
-              <div id="totalRevenueChart" class="px-2"></div>
+            <div class="card-header">
+              <ul class="nav nav-pills" role="tablist">
+                <li class="nav-item">
+                  <button
+                    type="button"
+                    class="nav-link active"
+                    role="tab"
+                    data-bs-toggle="tab"
+                    data-bs-target="#navs-tabs-line-card-income"
+                    aria-controls="navs-tabs-line-card-income"
+                    aria-selected="true"
+                  >
+                  Grafik Income
+                  </button>
+                </li>
+              </ul>
             </div>
-            <div class="col-md-4">
-              <div class="card-body">
-                <div class="text-center">
-                  <div class="dropdown">
-                    <button
-                      class="btn btn-sm btn-outline-primary dropdown-toggle"
-                      type="button"
-                      id="growthReportId"
-                      data-bs-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      2022
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="growthReportId">
-                      <a class="dropdown-item" href="javascript:void(0);">2021</a>
-                      <a class="dropdown-item" href="javascript:void(0);">2020</a>
-                      <a class="dropdown-item" href="javascript:void(0);">2019</a>
+            <div class="card-body px-0">
+              <div class="tab-content p-0">
+                <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
+                  <div class="d-flex p-4 pt-3">
+                    <div class="avatar flex-shrink-0 me-3">
+                      <img src="{{asset('dashboard/assets/img/icons/unicons/wallet.png')}}" alt="User" />
+                    </div>
+                    <div>
+                      <small class="text-muted d-block">Total Income</small>
+                      <div class="d-flex align-items-center">
+                        <h6 class="mb-0 me-1">@currency($countAllIncome)</h6>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-              <div id="growthChart"></div>
-              <div class="text-center fw-semibold pt-3 mb-2">62% Company Growth</div>
-
-              <div class="d-flex px-xxl-4 px-lg-2 p-4 gap-xxl-3 gap-lg-1 gap-3 justify-content-between">
-                <div class="d-flex">
-                  <div class="me-2">
-                    <span class="badge bg-label-primary p-2"><i class="bx bx-dollar text-primary"></i></span>
+                  {{-- income cart --}}
+                  <div id="incomeChartNew">
                   </div>
-                  <div class="d-flex flex-column">
-                    <small>2022</small>
-                    <h6 class="mb-0">$32.5k</h6>
-                  </div>
-                </div>
-                <div class="d-flex">
-                  <div class="me-2">
-                    <span class="badge bg-label-info p-2"><i class="bx bx-wallet text-info"></i></span>
-                  </div>
-                  <div class="d-flex flex-column">
-                    <small>2021</small>
-                    <h6 class="mb-0">$41.2k</h6>
-                  </div>
+                  {{-- income cart --}}
                 </div>
               </div>
             </div>
           </div>
-        </div>
       </div>
-      <!--/ Total Revenue -->
+      <!--/ Total Income -->
       <div class="col-12 col-md-8 col-lg-4 order-3 order-md-2">
         <div class="row">
           <div class="col-6 mb-4">
@@ -184,7 +170,37 @@
                     </div>
                   </div>
                 </div>
-                <span class="d-block mb-1">Payments</span>
+                <span class="d-block mb-1">Order Rejected</span>
+                <h3 class="card-title text-nowrap mb-2">$2,456</h3>
+                <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
+              </div>
+            </div>
+          </div>
+          <div class="col-6 mb-4">
+            <div class="card">
+              <div class="card-body">
+                <div class="card-title d-flex align-items-start justify-content-between">
+                  <div class="avatar flex-shrink-0">
+                    <img src="{{asset('dashboard/assets/img/icons/unicons/paypal.png')}}" alt="Credit Card" class="rounded" />
+                  </div>
+                  <div class="dropdown">
+                    <button
+                      class="btn p-0"
+                      type="button"
+                      id="cardOpt4"
+                      data-bs-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      <i class="bx bx-dots-vertical-rounded"></i>
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt4">
+                      <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                      <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                    </div>
+                  </div>
+                </div>
+                <span class="d-block mb-1">Order Rejected</span>
                 <h3 class="card-title text-nowrap mb-2">$2,456</h3>
                 <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
               </div>
@@ -214,7 +230,7 @@
                     </div>
                   </div>
                 </div>
-                <span class="fw-semibold d-block mb-1">Transactions</span>
+                <span class="fw-semibold d-block mb-1">Order Accepted</span>
                 <h3 class="card-title mb-2">$14,857</h3>
                 <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.14%</small>
               </div>
@@ -348,185 +364,67 @@
 
       <!-- Expense Overview -->
       <div class="col-md-6 col-lg-4 order-1 mb-4">
-        <div class="card h-100">
-          <div class="card-header">
-            <ul class="nav nav-pills" role="tablist">
-              <li class="nav-item">
-                <button
-                  type="button"
-                  class="nav-link active"
-                  role="tab"
-                  data-bs-toggle="tab"
-                  data-bs-target="#navs-tabs-line-card-income"
-                  aria-controls="navs-tabs-line-card-income"
-                  aria-selected="true"
-                >
-                  Income
-                </button>
-              </li>
-              <li class="nav-item">
-                <button type="button" class="nav-link" role="tab">Expenses</button>
-              </li>
-              <li class="nav-item">
-                <button type="button" class="nav-link" role="tab">Profit</button>
-              </li>
-            </ul>
-          </div>
-          <div class="card-body px-0">
-            <div class="tab-content p-0">
-              <div class="tab-pane fade show active" id="navs-tabs-line-card-income" role="tabpanel">
-                <div class="d-flex p-4 pt-3">
-                  <div class="avatar flex-shrink-0 me-3">
-                    <img src="{{asset('dashboard/assets/img/icons/unicons/wallet.png')}}" alt="User" />
-                  </div>
-                  <div>
-                    <small class="text-muted d-block">Total Balance</small>
-                    <div class="d-flex align-items-center">
-                      <h6 class="mb-0 me-1">$459.10</h6>
-                      <small class="text-success fw-semibold">
-                        <i class="bx bx-chevron-up"></i>
-                        42.9%
-                      </small>
-                    </div>
-                  </div>
-                </div>
-                <div id="incomeChart"></div>
-                <div class="d-flex justify-content-center pt-4 gap-2">
-                  <div class="flex-shrink-0">
-                    <div id="expensesOfWeek"></div>
-                  </div>
-                  <div>
-                    <p class="mb-n1 mt-1">Expenses This Week</p>
-                    <small class="text-muted">$39 less than last week</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+
       </div>
       <!--/ Expense Overview -->
 
-      <!-- Transactions -->
-      <div class="col-md-6 col-lg-4 order-2 mb-4">
-        <div class="card h-100">
-          <div class="card-header d-flex align-items-center justify-content-between">
-            <h5 class="card-title m-0 me-2">Transactions</h5>
-            <div class="dropdown">
-              <button
-                class="btn p-0"
-                type="button"
-                id="transactionID"
-                data-bs-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <i class="bx bx-dots-vertical-rounded"></i>
-              </button>
-              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="transactionID">
-                <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
-                <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
-                <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
-              </div>
-            </div>
-          </div>
-          <div class="card-body">
-            <ul class="p-0 m-0">
-              <li class="d-flex mb-4 pb-1">
-                <div class="avatar flex-shrink-0 me-3">
-                  <img src="{{asset('dashboard/assets/img/icons/unicons/paypal.png')}}" alt="User" class="rounded" />
-                </div>
-                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                  <div class="me-2">
-                    <small class="text-muted d-block mb-1">Paypal</small>
-                    <h6 class="mb-0">Send money</h6>
-                  </div>
-                  <div class="user-progress d-flex align-items-center gap-1">
-                    <h6 class="mb-0">+82.6</h6>
-                    <span class="text-muted">USD</span>
-                  </div>
-                </div>
-              </li>
-              <li class="d-flex mb-4 pb-1">
-                <div class="avatar flex-shrink-0 me-3">
-                  <img src="{{asset('dashboard/assets/img/icons/unicons/wallet.png')}}" alt="User" class="rounded" />
-                </div>
-                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                  <div class="me-2">
-                    <small class="text-muted d-block mb-1">Wallet</small>
-                    <h6 class="mb-0">Mac'D</h6>
-                  </div>
-                  <div class="user-progress d-flex align-items-center gap-1">
-                    <h6 class="mb-0">+270.69</h6>
-                    <span class="text-muted">USD</span>
-                  </div>
-                </div>
-              </li>
-              <li class="d-flex mb-4 pb-1">
-                <div class="avatar flex-shrink-0 me-3">
-                  <img src="{{asset('dashboard/assets/img/icons/unicons/chart.png')}}" alt="User" class="rounded" />
-                </div>
-                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                  <div class="me-2">
-                    <small class="text-muted d-block mb-1">Transfer</small>
-                    <h6 class="mb-0">Refund</h6>
-                  </div>
-                  <div class="user-progress d-flex align-items-center gap-1">
-                    <h6 class="mb-0">+637.91</h6>
-                    <span class="text-muted">USD</span>
-                  </div>
-                </div>
-              </li>
-              <li class="d-flex mb-4 pb-1">
-                <div class="avatar flex-shrink-0 me-3">
-                  <img src="{{asset('dashboard/assets/img/icons/unicons/cc-success.png')}}" alt="User" class="rounded" />
-                </div>
-                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                  <div class="me-2">
-                    <small class="text-muted d-block mb-1">Credit Card</small>
-                    <h6 class="mb-0">Ordered Food</h6>
-                  </div>
-                  <div class="user-progress d-flex align-items-center gap-1">
-                    <h6 class="mb-0">-838.71</h6>
-                    <span class="text-muted">USD</span>
-                  </div>
-                </div>
-              </li>
-              <li class="d-flex mb-4 pb-1">
-                <div class="avatar flex-shrink-0 me-3">
-                  <img src="{{asset('dashboard/assets/img/icons/unicons/wallet.png')}}" alt="User" class="rounded" />
-                </div>
-                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                  <div class="me-2">
-                    <small class="text-muted d-block mb-1">Wallet</small>
-                    <h6 class="mb-0">Starbucks</h6>
-                  </div>
-                  <div class="user-progress d-flex align-items-center gap-1">
-                    <h6 class="mb-0">+203.33</h6>
-                    <span class="text-muted">USD</span>
-                  </div>
-                </div>
-              </li>
-              <li class="d-flex">
-                <div class="avatar flex-shrink-0 me-3">
-                  <img src="{{asset('dashboard/assets/img/icons/unicons/cc-warning.png')}}" alt="User" class="rounded" />
-                </div>
-                <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                  <div class="me-2">
-                    <small class="text-muted d-block mb-1">Mastercard</small>
-                    <h6 class="mb-0">Ordered Food</h6>
-                  </div>
-                  <div class="user-progress d-flex align-items-center gap-1">
-                    <h6 class="mb-0">-92.45</h6>
-                    <span class="text-muted">USD</span>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
       <!--/ Transactions -->
     </div>
 </div>
 @endsection
+@section('extraJS')
+
+<script>
+    function number_format(number, decimals, dec_point, thousands_sep) {
+    number = (number + '').replace(',', '').replace(' ', '');
+    var n = !isFinite(+number) ? 0 : +number,
+        prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+        sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+        dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+        s = '',
+        toFixedFix = function(n, prec) {
+        var k = Math.pow(10, prec);
+        return '' + Math.round(n * k) / k;
+        };
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || '';
+        s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
+    }
+    var options = {
+          series: [{
+          name: 'Income',
+          data: [{{$getIncomeJanuary}}, {{$getIncomeFebruary}}, {{$getIncomeMarch}}, {{$getIncomeApril}}, {{$getIncomeMay}}, {{$getIncomeJune}}, {{$getIncomeJuly}}, {{$getIncomeAugust}}, {{$getIncomeSeptember}}, {{$getIncomeOctober}}, {{$getIncomeNovember}}, {{$getIncomeDecember}}]
+        },],
+          chart: {
+          height: 350,
+          type: 'area'
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+        yaxis: {
+            labels: {
+                formatter: function (value, index, values) {
+                return 'Rp. ' + number_format(value);
+                }
+            },
+        },
+        xaxis: {
+          categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        },
+        };
+
+        var chart = new ApexCharts(document.querySelector("#incomeChartNew"), options);
+        chart.render()
+</script>
+@endsection
+
