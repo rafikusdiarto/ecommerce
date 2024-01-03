b202b3307c75682655ff4466f478d9fd3a08e315
rafikusdiarto <rafikusdiarto1707@gmail.com>
2024-01-03 13:01:56 +0700

    fix:footer display

--- resources/views/user/components/footer.blade.php
+++ resources/views/user/components/footer.blade.php
@@ -1,4 +1,4 @@
-<section class="bg-footer bg-light">
+{{-- <section class="bg-footer bg-light">
     <div class="container">
         <div class="row">
             <div class="col-lg-4">
@@ -65,7 +65,7 @@
             </div>
         </div>
 
-        {{-- <div class="row mt-3">
+        <div class="row mt-3">
             <div class="col-lg-3">
                 <div class="client-images mt-4">
                     <img src="{{asset('users/assets/images/partner/1.png')}}" alt="logo-img" class="mx-auto img-fluid d-block">
@@ -86,7 +86,7 @@
                     <img src="{{asset('users/assets/images/partner/4.png')}}" alt="logo-img" class="mx-auto img-fluid d-block">
                 </div>
             </div>
-        </div> --}}
+        </div>
 
         <hr class="mt-5">
         <div class="row mt-4">
@@ -98,4 +98,113 @@
         </div>
 
     </div>
-</section>
+</section> --}}
+
+<div class="footer-area">
+    <div class="footer-container">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <!-- Start single blog -->
+                    <div class="col-md-6 col-lg-3 mb-md-30px mb-lm-30px">
+                        <div class="single-wedge">
+                            <div class="footer-logo">
+                                <a href="index.html"><img src="assets/images/logo/footer-logo.png" alt=""></a>
+                            </div>
+                            <p class="about-text">Discover your gaming. Shop the latest device.
+                        Your one-stop shop for all things gaming.
+                        Unleash your shopping desires.
+                            </p>
+                            <ul class="link-follow">
+                                <li>
+                                    <a class="m-0" title="Twitter" target="_blank" rel="noopener noreferrer" href="#"><i class="fa fa-facebook"
+                                        aria-hidden="true"></i></a>
+                                </li>
+                                <li>
+                                    <a title="Tumblr" target="_blank" rel="noopener noreferrer" href="#"><i class="fa fa-tumblr" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a title="Facebook" target="_blank" rel="noopener noreferrer" href="#"><i class="fa fa-twitter" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a title="Instagram" target="_blank" rel="noopener noreferrer" href="#"><i class="fa fa-instagram" aria-hidden="true"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- End single blog -->
+                    <!-- Start single blog -->
+                    <div class="col-md-6 col-lg-3 col-sm-6 mb-lm-30px pl-lg-60px">
+                        <div class="single-wedge">
+                            <h4 class="footer-herading">Services</h4>
+                            <div class="footer-links">
+                                <div class="footer-row">
+                                    <ul class="align-items-center">
+                                        <li class="li"><a class="single-link" href="my-account.html">My Account</a></li>
+                                        <li class="li"><a class="single-link" href="contact.html">Contact</a></li>
+                                        <li class="li"><a class="single-link" href="cart.html">Shopping cart</a></li>
+                                        <li class="li"><a class="single-link" href="shop-left-sidebar.html">Shop</a></li>
+                                        <li class="li"><a class="single-link" href="login.html">Services Login</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End single blog -->
+                    <!-- Start single blog -->
+                    <div class="col-md-6 col-lg-3 col-sm-6 mb-lm-30px pl-lg-40px">
+                        <div class="single-wedge">
+                            <h4 class="footer-herading">More Info</h4>
+                            <div class="footer-links">
+                                <div class="footer-row">
+                                    <ul class="align-items-center">
+                                        <li class="li"><a class="single-link" href="my-account.html">My Account</a></li>
+                                        <li class="li"><a class="single-link" href="contact.html">Contact</a></li>
+                                        <li class="li"><a class="single-link" href="cart.html">Shopping cart</a></li>
+                                        <li class="li"><a class="single-link" href="shop-left-sidebar.html">Shop</a></li>
+                                        <li class="li"><a class="single-link" href="login.html">Services Login</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End single blog -->
+                    <!-- Start single blog -->
+                    <div class="col-md-6 col-lg-3 col-sm-12">
+                        <div class="single-wedge">
+                            <h4 class="footer-herading">Resources</h4>
+                            <div class="footer-links">
+                                <!-- News letter area -->
+                                <p class="address">Address: Your Address Goes Here.</p>
+                                <p class="phone">Phone/Fax:<a href="tel:0123456789"> 0123456789</a></p>
+                                <p class="mail">Email:<a href="mailto:demo@example.com"> demo@example.com</a></p>
+                                <p class="mail"><a href="https://demo@example.com"> demo@example.com</a></p>
+                                <!-- News letter area  End -->
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End single blog -->
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="line-shape-top line-height-1">
+                    <div class="row flex-md-row-reverse align-items-center">
+                        <div class="col-md-6 text-center text-md-end">
+                            <div class="payment-mth"><a href="#"><img class="img img-fluid" src="{{asset('users/assets/images/icons/payment.png')}}" alt="payment-image"></a></div>
+                        </div>
+                        <div class="col-md-6 text-center text-md-start">
+                            <p class="copy-text"> © 2022 <strong>RMart</strong> Made With <i class="fa fa-heart"
+                                aria-hidden="true"></i> By <a class="company-name" href="https://themeforest.net/user/codecarnival/portfolio">
+                                    <strong> rafikusdiarto </strong></a>.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
